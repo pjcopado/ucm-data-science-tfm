b0cae0d28cd2b13bebb5ba07129ec3070185ad16
pjcopado <pjcopado@gmail.com>
2025-02-05 22:21:18 +0100

    add mocked streaming response

--- backend/src/app/chatbot/api/routes/__init__.py
+++ backend/src/app/chatbot/api/routes/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from . import chat, chat_message
+from . import chat, chat_message, chat_stream
 
 router = APIRouter()
 
 router.include_router(router=chat.router)
 router.include_router(router=chat_message.router)
+router.include_router(router=chat_stream.router)

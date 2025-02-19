241c00f529460c0a9c25a731fb1bd697e446aee3
antoniordrguez <antonio.rn98@gmail.com>
2025-02-19 20:07:47 +0000

    Update new model

---
+++ llm_generator/modules/check_question.py
@@ -0,0 +1,66 @@
+import re
+from .system_logger import Logger
+from .llm_handler import LLMHandler
+from .template import PromptTemplate
+
+logger = Logger("Check Question")
+
+
+class CheckQuestion:
+    def __init__(self, model_name="llama-3_2-3B-instruct-Q6_K_L"):
+        """
+        Inicializa el generador de respuestas utilizando LLMHandler con llama.cpp
+        """
+        # Instanciar otros modulos
+        self.prompt_template = PromptTemplate(model_name)
+
+        # Cargar otros params
+        self.system_prompt_file = "system_prompt-check_question.txt"
+        self.user_prompt_file = "user_prompt-check_question.txt"
+
+        # Cargar el modelo
+        system_prompt = str(
+            self.prompt_template.generate_prompt("system", self.system_prompt_file)
+        )
+        self.llm_handler = LLMHandler(model_name, system_prompt)
+
+    def generate_response(self, user_input):
+        """
+        Genera una respuesta basada en la pregunta del usuario y el resultado SQL.
+
+        Args:
+            user_question (str): Pregunta original del usuario.
+            sql_result (str | int | float): Resultado de la consulta SQL.
+            query_generate (str): Query generada por el modelo.
+
+        Returns:
+            str: Respuesta generada en lenguaje natural.
+        """
+        try:
+            prompt = self.prompt_template.generate_prompt(
+                "user", self.user_prompt_file, user_input=user_input
+            )
+
+            response = self.llm_handler.generate(prompt)
+
+            # Expresión regular flexible
+            logger.debug("Extracting checking question status from model output...")
+            rexpr = r"^\s*Answer\s*:?\s*"
+            answer = re.sub(rexpr, "", response).strip()
+
+            logger.info(f"Check question status: {answer}")
+
+            response = {"status": answer}
+
+            return response
+
+        except Exception as e:
+            response = {
+                "status": "KO",
+            }
+            logger.error(e)
+            return e, response
+
+    def close(self):
+        """Cierra la instancia de LLMHandler."""
+        self.llm_handler.close()
--- llm_generator/modules/sql_generator.py
+++ llm_generator/modules/sql_generator.py
@@ -7,6 +7,7 @@ from .embedding_manager import EmbeddingManager
 from .model_logger import ModelLogger
 from .llm_handler import LLMHandler
 from .system_logger import Logger
+from .check_question import CheckQuestion
 
 logger = Logger("Query Generator")
 
@@ -48,6 +49,10 @@ class SQLQueryGenerator:
         # Response status
         self.response_status_ok = "query_completed"
         self.response_status_ko = "query_failed"
+        self.response_status_invalid = "query_invalid"
+
+        # Cargar modelo check question
+        self.question_checker = CheckQuestion()
 
         # Cargar el modelo
         system_prompt = str(
@@ -56,7 +61,7 @@ class SQLQueryGenerator:
                 self.system_prompt_file,
                 user_input="",
                 user_instructions="",
-                db_schema=self.db_schema_and_relationships,
+                db_schema="",
             )
         )
         self.llm = LLMHandler(model_name, system_prompt)
@@ -82,119 +87,146 @@ class SQLQueryGenerator:
             user_instructions = "" if not user_instructions else user_instructions
             db_schema = self.db_schema_and_relationships if not db_schema else db_schema
 
-            while attempts < self.max_attempts:
-                if not errors:
-                    user_input_embedding = self.embedder.embed_text(user_input)
-                    similarity_list = self.model_logger.log_similarity_search(
-                        user_input_embedding,
-                        top_k=3,
-                        compare="user_input",
-                        status="OK",
-                        threshold=0.90,
-                    )
-                    logger.info(f"similarity_list: {similarity_list}")
+            question_checked = self.question_checker.generate_response(user_input)
+            if isinstance(question_checked, tuple):
+                e, _ = question_checked
+                status = self.response_status_ko
+                response = {
+                    "id": None,
+                    "query": None,
+                    "confidence_score": None,
+                    "status": status,
+                }
+                logger.error(e)
+                return e, response
 
-                    prompt = self.prompt_template.generate_prompt(
-                        "user",
-                        self.user_prompt_file,
-                        user_input,
-                        user_instructions,
-                        db_schema,
-                        similarity_list=similarity_list,
-                    )
+            if question_checked["status"] == "OK":
+                while attempts < self.max_attempts:
+                    if not errors:
+                        user_input_embedding = self.embedder.embed_text(user_input)
+                        similarity_list = self.model_logger.log_similarity_search(
+                            user_input_embedding,
+                            top_k=3,
+                            compare="user_input",
+                            status="OK",
+                            threshold=0.90,
+                        )
+                        logger.info(f"similarity_list: {similarity_list}")
 
-                elif errors and attempts == 1:
-                    prompt = self.prompt_template.generate_prompt_combined(
-                        "system",
-                        self.system_error_prompt_file,
-                        "user",
-                        self.user_error_prompt_file,
-                        user_input,
-                        user_instructions,
-                        db_schema,
-                        error_list=errors,
-                        initial_query=initial_query,
-                    )
+                        prompt = self.prompt_template.generate_prompt(
+                            "user",
+                            self.user_prompt_file,
+                            user_input,
+                            user_instructions,
+                            db_schema,
+                            similarity_list=similarity_list,
+                        )
 
-                else:
-                    prompt = self.prompt_template.generate_prompt(
-                        "user",
-                        self.user_error_prompt_file,
-                        user_input,
-                        user_instructions,
-                        db_schema,
-                        error_list=errors,
-                        initial_query=initial_query,
-                    )
+                    elif errors and attempts == 1:
+                        prompt = self.prompt_template.generate_prompt_combined(
+                            "system",
+                            self.system_error_prompt_file,
+                            "user",
+                            self.user_error_prompt_file,
+                            user_input,
+                            user_instructions,
+                            db_schema,
+                            error_list=errors,
+                            initial_query=initial_query,
+                        )
 
-                # Execute Model
-                start_time = time.time()
+                    else:
+                        prompt = self.prompt_template.generate_prompt(
+                            "user",
+                            self.user_error_prompt_file,
+                            user_input,
+                            user_instructions,
+                            db_schema,
+                            error_list=errors,
+                            initial_query=initial_query,
+                        )
 
-                # Generate query
-                query_text = self.llm.generate(prompt)
-                logger.info(f"Model output: {query_text}")
-                end_time = time.time()
-                execution_time = end_time - start_time
-                query = query_text
-                logger.info(f"Query generated. Execution time: {execution_time}")
+                    # Execute Model
+                    start_time = time.time()
 
-                # Validate query
-                logger.info(f"Validating query: {query}")
-                validation_results = self.query_manager.validate_sql_query(query)
+                    # Generate query
+                    query_text = self.llm.generate(prompt)
+                    logger.info(f"Model output: {query_text}")
+                    end_time = time.time()
+                    execution_time = end_time - start_time
+                    query = query_text
+                    logger.info(f"Query generated. Execution time: {execution_time}")
 
-                if validation_results["status"] == "OK":
-                    user_input_embedding = self.embedder.embed_text(user_input)
-                    query_embedding = self.embedder.embed_text(query)
-                    confidence_score = self.model_logger.log_get_confidence_score(
-                        query_embedding, compare="query", threshold=0.85
-                    )
+                    # Validate query
+                    logger.info(f"Validating query: {query}")
+                    validation_results = self.query_manager.validate_sql_query(query)
 
-                    # Write into log
-                    logger.info("Writing into log...")
-                    self.model_logger.log_evaluation(
-                        uuid,
-                        user_input,
-                        user_input_embedding,
-                        query,
-                        query_embedding,
-                        execution_time,
-                    )
+                    if validation_results["status"] == "OK":
+                        user_input_embedding = self.embedder.embed_text(user_input)
+                        query_embedding = self.embedder.embed_text(query)
+                        confidence_score = self.model_logger.log_get_confidence_score(
+                            query_embedding, compare="query", threshold=0.85
+                        )
 
-                    status = self.response_status_ok
+                        # Write into log
+                        logger.info("Writing into log...")
+                        self.model_logger.log_evaluation(
+                            uuid,
+                            user_input,
+                            user_input_embedding,
+                            query,
+                            query_embedding,
+                            execution_time,
+                        )
 
-                    logger.info(f"id: {uuid}")
-                    logger.info(f"query: {query}")
-                    logger.info(f"confidence_score: {confidence_score}")
-                    logger.info(f"status: {status}")
+                        status = self.response_status_ok
 
-                    response = {
-                        "id": uuid,
-                        "query": query,
-                        "confidence_score": confidence_score,
-                        "status": status,
-                    }
+                        logger.info(f"id: {uuid}")
+                        logger.info(f"query: {query}")
+                        logger.info(f"confidence_score: {confidence_score}")
+                        logger.info(f"status: {status}")
 
-                    return response
+                        response = {
+                            "id": uuid,
+                            "query": query,
+                            "confidence_score": confidence_score,
+                            "status": status,
+                        }
 
-                elif validation_results["status"] == "KO":
-                    initial_query = query
-                    errors.append(validation_results["message"])
+                        return response
 
-                attempts += 1
-                logger.info(f"Retrying... ({attempts}/{self.max_attempts})")
+                    elif validation_results["status"] == "KO":
+                        initial_query = query
+                        errors.append(validation_results["message"])
 
-            # "A valid query could not be generated. Can you rephrase the question?"
-            status = self.response_status_ko
-            response = {
-                "id": uuid,
-                "query": None,
-                "confidence_score": None,
-                "status": status,
-            }
-            logger.warn(
-                f"Could not generate a valid query after multiple attempts. Status: {status}"
-            )
-            return response
+                    attempts += 1
+                    logger.info(f"Retrying... ({attempts}/{self.max_attempts})")
+
+                # "A valid query could not be generated. Can you rephrase the question?"
+                status = self.response_status_ko
+                response = {
+                    "id": uuid,
+                    "query": None,
+                    "confidence_score": None,
+                    "status": status,
+                }
+                logger.warn(
+                    f"Could not generate a valid query after multiple attempts. Status: {status}"
+                )
+                if errors:
+                    logger.error(f"Errors trying generate a SQL query: {errors}")
+                return response
+
+            else:
+                status = self.response_status_invalid
+                response = {
+                    "id": uuid,
+                    "query": None,
+                    "confidence_score": None,
+                    "status": status,
+                }
+                logger.warn(f"Not a valid user question. Status: {status}")
+                return response
 
         except Exception as e:
             status = self.response_status_ko
